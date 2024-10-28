5402edf1feca05f9747583d3b42606619638febe
Patrick Hughes <phughes@simplextrading.com>
2024-10-28 13:38:15 -0500

    added comments

--- ix_allocation/ix_allocation.py
+++ ix_allocation/ix_allocation.py
@@ -1,6 +1,8 @@
 import os, re, time
 import pandas as pd
 
+# Filters all IDB broker files for brokers with Index activity
+
 os.chdir('C:/gdrive/Shared drives/accounting/Projects/Index Allocations')
 done = False
 while not done:
@@ -8,22 +10,25 @@ while not done:
         files = os.listdir()
         idb_files = filter(lambda x: re.search(r'\d{6}', x), files)
 
-
+        # grabbing and concat all IDB summary files
         master_df = pd.DataFrame()
         for i in idb_files:
             master_df = pd.concat([master_df, pd.read_csv(i)])
             master_df = master_df.reset_index(drop=True)
             
         master_df.sort_values('data_date')
-
+        
+        # Pivot to summarize trade fees by broker
         master_pivot = pd.pivot_table(master_df, 'trade_fee', 'broker', 'data_month', aggfunc='sum')
         master_pivot = master_pivot.reset_index()
         
         full_broker_list = list(master_pivot['broker'].values)
         
+        # Getting list of IX only brokers
         temp_master_pivot = master_pivot.loc[(master_pivot['broker'].str.endswith('X', na=False)) | (master_pivot['broker'].isin(['XFA', 'XFAA', 'XFAF', 'XFAL', 'XFAS', 'WEXF']))].copy(deep=True)
         ix_only_brokers = list(temp_master_pivot['broker'].values)
         
+        # Getting list of brokers with IX and IDB trades
         brokers_to_add_back = []
         for i in ix_only_brokers:
             if i[-1] == 'X':
@@ -36,6 +41,7 @@ while not done:
             else:
                 continue
         
+        # Filtering Main DF by IX brokers
         all_needed_brokers = ix_only_brokers + brokers_to_add_back
         needed_brokers_pivot = master_pivot.loc[master_pivot['broker'].isin(all_needed_brokers)]
         needed_brokers_pivot = needed_brokers_pivot.set_index('broker', drop=True)
