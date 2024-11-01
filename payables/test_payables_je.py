4db709bb5c4695fd139549fdcce374538649adbf
Patrick Hughes <phughes@simplextrading.com>
2024-11-01 12:30:59 -0500

    Added date string to save file name

--- payables/test_payables_je.py
+++ payables/test_payables_je.py
@@ -6,11 +6,13 @@ year = int(input('Year:\n>\t'))
 month = int(input('Month:\n>\t'))
 day = int(input('Day:\n>\t'))
 
-payables = JECreator(datetime(year, month, day))
+batch_date = datetime(year, month, day)
+
+payables = JECreator(batch_date)
 
 invoices, vendors, coas = payables.file_getter()
 
 bill_dfs = payables.initiator(payables=invoices, vendor_mapping=vendors, account_mappings=coas)
 
 for i in bill_dfs.keys():
-    bill_dfs[i].to_csv(f'C:/Users/phughes_simplextradi/Downloads/{i} Bills.csv', index=False)
+    bill_dfs[i].to_csv(f'C:/Users/phughes_simplextradi/Downloads/{i} {batch_date.strftime('%Y-%m-%d')} Bills.csv', index=False)
