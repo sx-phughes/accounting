450dc315476a140080bc24b9f9d551f90218d47c
Patrick Hughes <phughes@simplextrading.com>
2024-11-06 17:17:23 -0600

    bug fix to pm month files in abn_month_end test

--- abn_month_end/test.py
+++ abn_month_end/test.py
@@ -6,15 +6,15 @@ year = int(input('Closing month year:\n>\t'))
 
 now = AbnMonthEnd.AbnMonthEnd(year,month)
 cm_cash, cm_position = now.grab_files(year, month)
-pm_cash, pm_position = now.grab_files(2024, 7)
+pm_cash, pm_position = now.grab_files(2024, month-1)
 ledger_mapping, account_mapping = now.get_mapping_files()
 data_df = now.data_tab(cm_cash, pm_cash, ledger_mapping, account_mapping)
 
-data_df.to_csv('./data_df_test.csv')
+data_df.to_csv('./data_df_test.csv', index=False)
 
 positions, pivot, categories = now.positions_tab(cm_position)
 
-save_stem = 'C:/gdrive/Shared drives/accounting/Simplex Trading/2024/202409/ABN'
+save_stem = f'C:/gdrive/Shared drives/accounting/Simplex Trading/{str(year)}/{str(year)+str(month)}/ABN'
 
 positions.to_csv(save_stem + '/positions_df_test.csv')
 pivot.to_csv(save_stem + '/positions-pivot_df_test.csv')
