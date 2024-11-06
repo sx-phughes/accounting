d2dd57cbff80b710cb93a5a9d0359b23c96acd82
Patrick Hughes <phughes@simplextrading.com>
2024-11-06 16:05:54 -0600

    uncommented data df files in abn_month_end

--- abn_month_end/test.py
+++ abn_month_end/test.py
@@ -6,11 +6,11 @@ year = int(input('Closing month year:\n>\t'))
 
 now = AbnMonthEnd.AbnMonthEnd(year,month)
 cm_cash, cm_position = now.grab_files(year, month)
-# pm_cash, pm_position = now.grab_files(2024, 7)
-# ledger_mapping, account_mapping = now.get_mapping_files()
-# data_df = now.data_tab(cm_cash, pm_cash, ledger_mapping, account_mapping)
+pm_cash, pm_position = now.grab_files(2024, 7)
+ledger_mapping, account_mapping = now.get_mapping_files()
+data_df = now.data_tab(cm_cash, pm_cash, ledger_mapping, account_mapping)
 
-# data_df.to_csv('./data_df_test.csv')
+data_df.to_csv('./data_df_test.csv')
 
 positions, pivot, categories = now.positions_tab(cm_position)
 
