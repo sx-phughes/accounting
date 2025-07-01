6282d6649d9857276512ab313b8b037893a7c5c8
Patrick Hughes <phughes@simplextrading.com>
2025-07-01 12:16:10 -0500

    began messing around with a textual app

--- textual_app/__main__.py
+++ textual_app/__main__.py
@@ -1,13 +1,22 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Welcome, Button
+from textual.events import Key
+from textual.widgets import Welcome, Button, Label, Header
 
 class MyApp(App):
-    def on_key(self) -> None:
-        self.mount(Welcome())
-        self.query_one(Button).label = "Yes!"
+    TITLE = "A Question App"
+    SUB_TITLE = "whoooo meee??"
+    def compose (self) -> ComposeResult:
+        yield Header()
+        yield Label("Do you love Textual?", id="question")
+        yield Button("Yes", id="yes", variant="primary")
+        yield Button("No", id="no", variant="error")
 
     def on_mount(self) -> None:
         self.screen.styles.background = "darkblue"
+    
+    def on_key(self, event: Key):
+        self.title = event.key
+        self.sub_title = f"You just pressed {event.key}!"
 
 if __name__ == "__main__":
     app = MyApp()
