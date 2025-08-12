4129de7f7758d852993f2f38632c98a5483f8b06
Patrick Hughes <phughes@simplextrading.com>
2025-08-12 12:48:22 -0500

    added type setting function

--- payables/Interface/functions.py
+++ payables/Interface/functions.py
@@ -1,5 +1,7 @@
 # Standard imports
+import numpy as np
 import re
+from typing import Any
 
 def check_date(date: str):
     """Check that date matches YYYY-MM-DD format"""
@@ -11,3 +13,17 @@ def check_date(date: str):
             int(match.group(3)) in range(1, 32):
             return True
     return False
+
+def set_type(obj: Any, dest_type: str) -> Any:
+    if dest_type == "str":
+        return str(obj)
+    elif dest_type == "int":
+        return int(obj)
+    elif dest_type == "float":
+        return float(obj)
+    elif dest_type == "float64":
+        return np.float64(obj)
+    elif dest_type == "bool":
+        return bool(obj)
+    else:
+        raise TypeError(f"Unprogrammed type: {dest_type}")
