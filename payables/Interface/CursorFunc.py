fdcac43f86a4bb814b6405225d3ecddf7b356ad6
Patrick Hughes <phughes@simplextrading.com>
2025-09-22 10:08:02 -0500

    Adjusted clear_end_of_line_after_input to not clear if input has no len i.e. reused old input

--- payables/Interface/CursorFunc.py
+++ payables/Interface/CursorFunc.py
@@ -3,40 +3,46 @@ from ctypes import wintypes
 import re
 import sys
 
+
 def cursor_up():
     """Moves the cursor up one row after input."""
     sys.stdout.flush()
     # clear line nav'ed from
     clear_end_of_line_after_input("")
     # undo \n from entering input
-    print("\033[A", end='', flush=True)
+    print("\033[A", end="", flush=True)
     # move up a line and to beginning
-    print("\033[1F", end='', flush=True)
+    print("\033[1F", end="", flush=True)
+
 
 def cursor_down():
     """Moves the cursor down one row after input."""
     clear_end_of_line_after_input()
     return
 
+
 def print_sugg_value(value: str) -> None:
     """Reprint a string to a line and moves cursor to beginning of string."""
 
     input_len = len(str(value))
-    print(value, end='', flush=True)
+    print(value, end="", flush=True)
     # Erase from cursor to end of line
-    print("\033[0K", end='', flush=True)
+    print("\033[0K", end="", flush=True)
     # Move cursor to beginning of the response
-    print(f"\033[{input_len}D", end='', flush=True)
+    print(f"\033[{input_len}D", end="", flush=True)
+
 
 def clear_end_of_line_after_input(value: str) -> None:
     """After inputting a value, clears line to end of screen. Returns cursor
     to beginning of next line."""
 
     data_len = len(value)
-    print("\033[A", end='', flush=True)
-    print(f"\033[{20+data_len}C", end='', flush=True)
-    print("\033[0K", end='', flush=True)
-    print(f"\033[E", end='', flush=True)
+    if data_len > 0:
+        print("\033[A", end="", flush=True)
+        print(f"\033[{20+data_len}C", end="", flush=True)
+        print("\033[0K", end="", flush=True)
+        print(f"\033[E", end="", flush=True)
+
 
 def get_cursor_pos() -> tuple[int, int]:
     """Returns the cursor position in the terminal."""
@@ -47,12 +53,12 @@ def get_cursor_pos() -> tuple[int, int]:
     kernel32.GetConsoleMode(
         kernel32.GetStdHandle(-10), ctypes.byref(old_stdin_mode)
     )
-    kernel32.SetConsoleMode(kernel32.GetStdHandle(-10),0)
+    kernel32.SetConsoleMode(kernel32.GetStdHandle(-10), 0)
     kernel32.GetConsoleMode(
         kernel32.GetStdHandle(-11), ctypes.byref(old_stdout_mode)
     )
     kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
-    
+
     try:
         buff = ""
         sys.stdout.write("\x1b[6n")
@@ -64,16 +70,16 @@ def get_cursor_pos() -> tuple[int, int]:
     finally:
         kernel32.SetConsoleMode(kernel32.GetStdHandle(-10), old_stdin_mode)
         kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), old_stdout_mode)
-    
+
     res = re.match(r"\x1b\[(?P<y>\d*);(?P<x>\d*)R", buff)
     if res:
         y = res.group("y")
         x = res.group("x")
         return (int(x), int(y))
 
+
 def better_input(prompt: str) -> str:
     """Input that undoes the automatic enter after submitting the value."""
 
     ret = input(prompt)
     ret_len = len(ret)
-    
