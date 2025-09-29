ecbbea9872980059cb616364cc0bf60cb5ca234e
Patrick Hughes <phughes@simplextrading.com>
2025-09-29 16:09:00 -0500

    better formatting of resultant json

--- vendor_to_json.py
+++ vendor_to_json.py
@@ -8,3 +8,17 @@ df = pd.read_excel(io=path, sheet_name=sheet)
 df.to_json(
     path_or_buf="./vendors.json", orient="records", index=False, lines=True
 )
+
+vendors_txt = ""
+
+with open("./vendors.json", "r") as file:
+    vendors_txt = file.read()
+
+
+split = vendors_txt.split("}")
+joined = "},".join(split)
+
+final = "{" + joined + "}"
+
+with open("./formatted_vendors.json", "w") as f:
+    f.write(final)
