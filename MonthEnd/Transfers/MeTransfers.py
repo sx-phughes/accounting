e5fca461e12106dab1e98c72e5be6b9759b02179
Patrick Hughes <phughes@simplextrading.com>
2025-11-04 10:11:00 -0600

    Added accounts SIM10 - SIM12 to monthly sweeps

--- MonthEnd/Transfers/MeTransfers.py
+++ MonthEnd/Transfers/MeTransfers.py
@@ -3,26 +3,30 @@ from MonthEnd.Transfers.TransferTableSetup import *
 from datetime import datetime, timedelta
 import os
 
+
 def baml_path(date: datetime):
-    
-    path = f'C:/gdrive/My Drive/BAML/Account Mgmt Analysis Reports/Account Management Analysis Report-64499-{date.strftime('%m-%d-%Y')}.pdf'
-    
+
+    path = f"C:/gdrive/My Drive/BAML/Account Mgmt Analysis Reports/Account Management Analysis Report-64499-{date.strftime('%m-%d-%Y')}.pdf"
+
     return path
 
-def abn_date(date: datetime):
-    return date.strftime('%Y%m%d')
 
-def zero_pad(string: str, t_length: int, before: bool=True):
+def abn_date(date: datetime):
+    return date.strftime("%Y%m%d")
+
+
+def zero_pad(string: str, t_length: int, before: bool = True):
     num_zeros = t_length - len(string)
-    
+
     if before:
-        string = '0' * num_zeros + string
+        string = "0" * num_zeros + string
     else:
-        string += '0' * num_zeros
-        
+        string += "0" * num_zeros
+
     return string
 
-def run_baml_table(date: datetime, save_path = '.'):
+
+def run_baml_table(date: datetime, save_path="."):
     path = baml_path(date)
 
     # Get BAML Month-end Data
@@ -31,19 +35,16 @@ def run_baml_table(date: datetime, save_path = '.'):
     row_data = []
 
     baml_accounts = [
-        '644-40300-D4',
-        '644-40846-D6',
-        '644-40865-D6',
-        '644-41315-D1',
-        '644-99865-D6'
+        "644-40300-D4",
+        "644-40846-D6",
+        "644-40865-D6",
+        "644-41315-D1",
+        "644-99865-D6",
     ]
 
     for i, row in df.iterrows():
-        if row['Account No.'] in baml_accounts:
-            i_row = [
-                row['Account No.'],
-                round(float(row['Equity']),2)
-            ]
+        if row["Account No."] in baml_accounts:
+            i_row = [row["Account No."], round(float(row["Equity"]), 2)]
             row_data.append(i_row)
 
     tfr_date = date + timedelta(-1)
@@ -54,58 +55,59 @@ def run_baml_table(date: datetime, save_path = '.'):
         print(i)
         baml_table.add_data_row(i[0], i[1])
 
-
-
     baml_table.add_final_row()
 
     baml_table.to_csv(
-        save_path + f'/{date.strftime('%Y%m%d')} BofA Transfers.csv',
-        index=False
+        save_path + f"/{date.strftime('%Y%m%d')} BofA Transfers.csv",
+        index=False,
     )
-    
-    
-def run_abn_tables(date: datetime, save_path = '.'):
+
+
+def run_abn_tables(date: datetime, save_path="."):
     date_str = abn_date(date)
-    
+
     abn_eqt_accounts = (
-        '008MMXV',      # MM    VIX Options
-        '695M526',      # MM    NXM SPY Stock and ETFs
-        '695M622',      # MM    Main MM Account
-        '695M679',      # MM    NXM IWM, QQQ stock and ETFs
-        '695M904',      # Desk  Tom O'Donnell
-        '695MMXZ',      # MM    NXM SPX Boxes
-        '813M473',      # MM    XM IWM, QQQ Options
-        '813M758'       # XM    SPY and SPX Options
+        "008MMXV",  # MM    VIX Options
+        "695M526",  # MM    NXM SPY Stock and ETFs
+        "695M622",  # MM    Main MM Account
+        "695M679",  # MM    NXM IWM, QQQ stock and ETFs
+        "695M904",  # Desk  Tom O'Donnell
+        "695MMXZ",  # MM    NXM SPX Boxes
+        "813M473",  # MM    XM IWM, QQQ Options
+        "813M758",  # XM    SPY and SPX Options
     )
 
     abn_fut_accounts = (
-        '6901SIMP3',    # Prop  Futures (Elliot)
-        '6901SIMP4',    # Prop  Euro/SOFR
-        '6901SIMP8',    # Desk  Tom O'Donnell
-#       '6901SIMP9',    # Prop  JJ Futures
-        '8131SIMP1',    # MM    XM VIX Futures
-        '8131SIMP2',    # MM    XM EMini Futures
-        '8131SIMP7'     # MM    XM NQ, RTY Futures
+        "6901SIMP3",  # Prop  Futures (Elliot)
+        "6901SIMP4",  # Prop  Euro/SOFR
+        "6901SIMP8",  # Desk  Tom O'Donnell
+        #       '6901SIMP9',    # Prop  JJ Futures
+        "6901SIM10",
+        "6901SIM11",
+        "6901SIM12",
+        "8131SIMP1",  # MM    XM VIX Futures
+        "8131SIMP2",  # MM    XM EMini Futures
+        "8131SIMP7",  # MM    XM NQ, RTY Futures
     )
-    
-    sixnine_tfr_table = AbnOptTransferTable('695')
-    eightone_tfr_table = AbnOptTransferTable('813')
+
+    sixnine_tfr_table = AbnOptTransferTable("695")
+    eightone_tfr_table = AbnOptTransferTable("813")
     futures_tfr_table = AbnFutTransferTable()
-    et_tfr_table = AbnOptTransferTable('ET')    
-    
-    eqt_me_stmt, fut_me_stmt = AbnMonthEndStatements(date.strftime('%Y%m%d'))
+    et_tfr_table = AbnOptTransferTable("ET")
+
+    eqt_me_stmt, fut_me_stmt = AbnMonthEndStatements(date.strftime("%Y%m%d"))
     et_table = EtMonthEnd(date.year, date.month)
-    
+
     # eqt_data = {acct: eqt_me_stmt.loc[eqt_me_stmt['ACCOUNT'] == acct,
     # 'EQUITY'].values[0] for acct in abn_eqt_accounts}
     # fut_data = {acct: fut_me_stmt.loc[fut_me_stmt['Account'] == acct,
     # 'NetLiq'].values[0] for acct in abn_fut_accounts}
-    
+
     eqt_data = {}
     fut_data = {}
     groups = [
         [eqt_me_stmt, abn_eqt_accounts, eqt_data, "ACCOUNT", "EQUITY"],
-        [fut_me_stmt, abn_fut_accounts, fut_data, "Account", "NetLiq"]
+        [fut_me_stmt, abn_fut_accounts, fut_data, "Account", "NetLiq"],
     ]
 
     for cat in groups:
@@ -122,33 +124,28 @@ def run_abn_tables(date: datetime, save_path = '.'):
                 data.update({account: value})
             except:
                 continue
-        
-    
+
     table_dict = {
-        '695': sixnine_tfr_table,
-        '813': eightone_tfr_table,
-        'Fut': futures_tfr_table
+        "695": sixnine_tfr_table,
+        "813": eightone_tfr_table,
+        "Fut": futures_tfr_table,
     }
-    
+
     acct_mapping = {
-        '695': abn_eqt_accounts,
-        '813': abn_eqt_accounts,
-        'Fut': abn_fut_accounts
+        "695": abn_eqt_accounts,
+        "813": abn_eqt_accounts,
+        "Fut": abn_fut_accounts,
     }
-    
-    data_ref = {
-        '695': eqt_data,
-        '813': eqt_data,
-        'Fut': fut_data
-    }
-    
+
+    data_ref = {"695": eqt_data, "813": eqt_data, "Fut": fut_data}
+
     f_names = {
-        '695': ' - ABN - 695.csv',
-        '813': ' - ABN - 813.csv',
-        'Fut': ' - ABN - Futures.csv',
-        'ET': ' - ABN - ETs.csv'
+        "695": " - ABN - 695.csv",
+        "813": " - ABN - 813.csv",
+        "Fut": " - ABN - Futures.csv",
+        "ET": " - ABN - ETs.csv",
     }
-    
+
     for acct_group in table_dict.keys():
         for i in acct_mapping[acct_group]:
             cond1 = i[0:3] == acct_group
@@ -168,25 +165,35 @@ def run_abn_tables(date: datetime, save_path = '.'):
                     continue
             else:
                 continue
-            
-            
-        if acct_group != 'Fut':
+
+        if acct_group != "Fut":
             table_dict[acct_group].add_final_row()
-    
-    for acct in et_table['Account']:
-        et_tfr_table.add_data_row(acct, et_table.loc[et_table['Account'] == acct, 'Transfer'].values[0], flip=False)
-        
+
+    for acct in et_table["Account"]:
+        et_tfr_table.add_data_row(
+            acct,
+            et_table.loc[et_table["Account"] == acct, "Transfer"].values[0],
+            flip=False,
+        )
+
     et_tfr_table.add_final_row()
-           
-    eightone_tfr_table['Firm'] = eightone_tfr_table['Firm'].apply(lambda x: zero_pad(x, 3))    
-    
+
+    eightone_tfr_table["Firm"] = eightone_tfr_table["Firm"].apply(
+        lambda x: zero_pad(x, 3)
+    )
+
     for acct_group in table_dict.keys():
-        table_dict[acct_group].to_csv(f'{save_path}/{date_str}{f_names[acct_group]}', index=False)
-    
-    et_tfr_table.to_csv(f'{save_path}/{date_str}{f_names['ET']}', index=False)
-        
-def run_ME_Transfers(year, month, day, save_path=os.environ['HOMEPATH'] + '\\Downloads'):
+        table_dict[acct_group].to_csv(
+            f"{save_path}/{date_str}{f_names[acct_group]}", index=False
+        )
+
+    et_tfr_table.to_csv(f"{save_path}/{date_str}{f_names['ET']}", index=False)
+
+
+def run_ME_Transfers(
+    year, month, day, save_path=os.environ["HOMEPATH"] + "\\Downloads"
+):
     dt = datetime(int(year), int(month), int(day))
-    
+
     run_abn_tables(dt, save_path)
     run_baml_table(dt, save_path)
--- MonthEnd/Transfers/TransferTableSetup.py
+++ MonthEnd/Transfers/TransferTableSetup.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime
 
+
 class TransferTable(pd.DataFrame):
     def __init__(self, col_names: list[str]):
         super().__init__(columns=col_names)
@@ -10,26 +11,27 @@ class TransferTable(pd.DataFrame):
     def add_row(self, data: list):
         self.loc[len(self)] = data
 
+
 class BamlTransferTable(TransferTable):
     cols = [
-            'Firm',
-            'TOE',
-            'Debit_Credit',
-            'Symbol',
-            'Qty',
-            'AMOUNT',
-            'ACCOUNT',
-            'Settle_Date',
-            'TRAILER1',
-            'TRAILER2',
-            'TRAILER3',
-            'Memo_CD',
-            'Offset_CD',
-            'Expiration_Date',
-            'Call_Put',
-            'Strike_Price'
-        ]
-    
+        "Firm",
+        "TOE",
+        "Debit_Credit",
+        "Symbol",
+        "Qty",
+        "AMOUNT",
+        "ACCOUNT",
+        "Settle_Date",
+        "TRAILER1",
+        "TRAILER2",
+        "TRAILER3",
+        "Memo_CD",
+        "Offset_CD",
+        "Expiration_Date",
+        "Call_Put",
+        "Strike_Price",
+    ]
+
     def __init__(self, transfer_date: str):
 
         super().__init__(col_names=BamlTransferTable.cols)
@@ -38,63 +40,95 @@ class BamlTransferTable(TransferTable):
     @property
     def settle_date(self):
         return self._settle_date
-    
+
     @settle_date.setter
     def settle_date(self, date):
         self._settle_date = date
-        self.dt_settle_date = datetime.strptime(date, '%m/%d/%Y')
+        self.dt_settle_date = datetime.strptime(date, "%m/%d/%Y")
         self.comment_date_str = datetime(
-            self.dt_settle_date.year,
-            self.dt_settle_date.month,
-            1
-        ).strftime('%b %Y')
-    
-    def add_data_row(self, account_no: str, amount: float, user: str = 'KPIE'):
-        account_no = account_no.replace('-','')
+            self.dt_settle_date.year, self.dt_settle_date.month, 1
+        ).strftime("%b %Y")
 
-        dc = 'D' if amount >= 0 else 'C'
+    def add_data_row(self, account_no: str, amount: float, user: str = "KPIE"):
+        account_no = account_no.replace("-", "")
 
-        row = [1, 'JE', dc, '', '', abs(amount), account_no, self.settle_date, f'{self.comment_date_str} CLOSE OUT', user, '', '', '', '' , '', '']
-        
+        dc = "D" if amount >= 0 else "C"
+
+        row = [
+            1,
+            "JE",
+            dc,
+            "",
+            "",
+            abs(amount),
+            account_no,
+            self.settle_date,
+            f"{self.comment_date_str} CLOSE OUT",
+            user,
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+        ]
+
         self.add_row(row)
 
         self.je_total += amount
-    
-    def add_final_row(self, user: str = 'KPIE'):
-        dc = 'C' if self.je_total >= 0 else 'D'
-        row = [1, 'JE', dc, '', '', round(abs(self.je_total),2), '64499821D9', self.settle_date, f'{self.comment_date_str} CLOSE OUT', user, '', '', '', '' , '', '']
-        
+
+    def add_final_row(self, user: str = "KPIE"):
+        dc = "C" if self.je_total >= 0 else "D"
+        row = [
+            1,
+            "JE",
+            dc,
+            "",
+            "",
+            round(abs(self.je_total), 2),
+            "64499821D9",
+            self.settle_date,
+            f"{self.comment_date_str} CLOSE OUT",
+            user,
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+        ]
+
         self.add_row(row)
 
 
 class AbnOptTransferTable(TransferTable):
     headers = [
-            'Code',
-            'CA',
-            'Firm',
-            'Origin',
-            'Account',
-            'SubAccount',
-            'Amount',
-            'Placement',
-            'CurrencyCode',
-            'AsOfDate',
-            'Bank',
-            'Check',
-            'CostCenter',
-            'Description'
-        ]
-    
+        "Code",
+        "CA",
+        "Firm",
+        "Origin",
+        "Account",
+        "SubAccount",
+        "Amount",
+        "Placement",
+        "CurrencyCode",
+        "AsOfDate",
+        "Bank",
+        "Check",
+        "CostCenter",
+        "Description",
+    ]
+
     offsets = {
-            '695': ['695', 'C', '10S'],
-            'ET': ['695', 'C', '10S'],
-            '813': ['813', '1', 'SICSH']
-        }
-    
+        "695": ["695", "C", "10S"],
+        "ET": ["695", "C", "10S"],
+        "813": ["813", "1", "SICSH"],
+    }
+
     def __init__(self, tfr_type: str):
         super().__init__(AbnOptTransferTable.headers)
         self.type = tfr_type
-        
+
     def add_data_row(self, account, amount, flip=True):
         if flip:
             new_amount = amount * -1
@@ -102,90 +136,94 @@ class AbnOptTransferTable(TransferTable):
         else:
             new_amount = amount
             self.je_total -= amount
-            
+
         row = [
-            '',
-            'A',
+            "",
+            "A",
             str(account[0:3]),
             account[3:4],
             account[4:],
-            '',
+            "",
             new_amount,
-            '10001',
-            'USD',
-            '',
-            '',
-            '',
-            '',
-            'Transfer'
+            "10001",
+            "USD",
+            "",
+            "",
+            "",
+            "",
+            "Transfer",
         ]
 
         self.add_row(row)
-    
+
     def add_final_row(self):
         row = [
-            '',
-            'A',
+            "",
+            "A",
             AbnOptTransferTable.offsets[self.type][0],
             AbnOptTransferTable.offsets[self.type][1],
             AbnOptTransferTable.offsets[self.type][2],
-            '',
+            "",
             self.je_total,
-            '10001',
-            'USD',
-            '',
-            '',
-            '',
-            '',
-            'Transfer'
+            "10001",
+            "USD",
+            "",
+            "",
+            "",
+            "",
+            "Transfer",
         ]
         self.add_row(row)
 
+
 class AbnFutTransferTable(TransferTable):
     headers = [
-            'Code',
-            'Firm',
-            'Office',
-            'PrimaryAccount',
-            'AT',
-            'JournalAmount',
-            'CC',
-            'Description',
-            'OffsetOffice',
-            'OffsetAccount',
-            'Offset_AT',
-            'Blank'
-        ]
-    
+        "Code",
+        "Firm",
+        "Office",
+        "PrimaryAccount",
+        "AT",
+        "JournalAmount",
+        "CC",
+        "Description",
+        "OffsetOffice",
+        "OffsetAccount",
+        "Offset_AT",
+        "Blank",
+    ]
+
     offsets = {
-            'SIMP1': 'SICSH',
-            'SIMP2': 'SICSH',
-            'SIMP7': 'SICSH',
-            'SIMP4': 'SIMP6',
-            'SIMP3': 'SIMP6',
-            'SIMP9': 'SIMP6'
-        }
-    
+        "SIMP1": "SICSH",
+        "SIMP2": "SICSH",
+        "SIMP7": "SICSH",
+        "SIMP4": "SIMP6",
+        "SIMP3": "SIMP6",
+        "SIMP9": "SIMP6",
+        "SIM10": "SIMP6",
+        "SIM11": "SIMP6",
+        "SIM12": "SIMP6",
+    }
+
     def __init__(self):
         super().__init__(col_names=AbnFutTransferTable.headers)
 
     def add_data_row(self, account: str, amount: float):
-        
+
         short_account = account[4:]
-        
+
         row = [
-            '',
-            'X',
-            '',
+            "",
+            "X",
+            "",
             short_account,
-            'RU',
+            "RU",
             amount * -1,
-            '',
-            'Journal Transfer',
-            '',
+            "",
+            "Journal Transfer",
+            "",
             self.offsets[short_account],
-            'RU',
-            ''
+            "RU",
+            "",
         ]
 
         self.add_row(row)
