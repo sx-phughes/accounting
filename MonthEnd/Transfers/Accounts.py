e996f4a4ce6c4842560a114415272832f0954fa4
Patrick Hughes <phughes@simplextrading.com>
2025-11-05 10:08:38 -0600

    Moved accounts to be swept into a central file

---
+++ MonthEnd/Transfers/Accounts.py
@@ -0,0 +1,33 @@
+abn_eqt_accounts = (
+    "008MMXV",  # MM    VIX Options
+    "695M526",  # MM    NXM SPY Stock and ETFs
+    "695M622",  # MM    Main MM Account
+    "695M679",  # MM    NXM IWM, QQQ stock and ETFs
+    "695M904",  # Desk  Tom O'Donnell
+    "695CS1V",  # Desk  Tom O'Donnell
+    "695MMXZ",  # MM    NXM SPX Boxes
+    "813M473",  # MM    XM IWM, QQQ Options
+    "813M758",  # XM    SPY and SPX Options
+)
+
+abn_fut_accounts = (
+    "6901SIMP3",  # Prop  Futures (Elliot)
+    "6901SIMP4",  # Prop  Euro/SOFR
+    "6901SIMP8",  # Desk  Tom O'Donnell
+    #       '6901SIMP9',    # Prop  JJ Futures
+    "6901SIM10",
+    "6901SIM11",
+    "6901SIM12",
+    "8131SIMP1",  # MM    XM VIX Futures
+    "8131SIMP2",  # MM    XM EMini Futures
+    "8131SIMP7",  # MM    XM NQ, RTY Futures
+)
+
+baml_accounts = (
+    "644-40300-D4",
+    "644-06300-D6",
+    "644-40846-D6",
+    "644-40865-D6",
+    "644-41315-D1",
+    "644-99865-D6",
+)
