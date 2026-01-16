025c480ea1556d073d2d265f02d2a3c03edf6bbe
Patrick Hughes <phughes@simplextrading.com>
2026-01-16 12:43:11 -0600

    short file collector script for audit purposes

---
+++ fileCollector.py
@@ -0,0 +1,23 @@
+import shutil
+import os
+import re
+
+src1 = "C:/gdrive/Shared drives/accounting/Simplex Trading/"
+src2 = "2025"
+src3 = "BOFA"
+src = "/".join([src1, src2, src3])
+
+file_pattern = r"\d{6} - BOFA Journal Entries - PQ.xlsm"
+
+dest1 = "C:/gdrive/Shared drives/accounting/Simplex Trading/Audit/2025"
+dest2 = "2025 Year End Fieldwork/33 - BOA Broker Statement Recon"
+dest = "/".join([dest1, dest2])
+
+for folder in os.listdir(src):
+    print(folder)
+    month_folder = "/".join([src, folder])
+    files = os.listdir(month_folder)
+    for file in files:
+        print("\t%s" % file)
+        if re.match(file_pattern, file):
+            shutil.copy(src="/".join([month_folder, file]), dst=dest)
