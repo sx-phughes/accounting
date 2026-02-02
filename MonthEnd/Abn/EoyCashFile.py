11bb180fb9354e9b0b2456ce02d5c3dc6faac05f
Patrick Hughes <phughes@simplextrading.com>
2026-02-02 15:50:33 -0600

    debugging to get routine to a workable format

--- MonthEnd/Abn/EoyCashFile.py
+++ MonthEnd/Abn/EoyCashFile.py
@@ -1,32 +1,54 @@
 import os
+import sys
 import pandas as pd
 import re
 import pypdf
-from MonthEnd.Abn.Base import get_archive_date_path
-from MonthEnd.Abn.EoyExtractPageText import *
 
-def convert_to_eoy_cash(year: int, pm_cash: pd.DataFrame):
-    ##### PROCESS NOTES #####
-    # MARK TO MARKET OPTIONS LONG/SHORT ARE NETTED TO MARK TO MARKET OPTIONS
-    # MARK TO MARKET UNSETTLED LONG/SHORT STOCK ARE NETTED TO MARK TO MARKET UNSETTLED STOCK
-    # MARK TO MARKET UNSETTLED LONG/SHORT PREFSTOCK ARE NETTED TO MARK TO MARKET UNSETTLED PREFSTOCK
-    #   ADD PRIOR VALUES FOR THESE TO THE RESULTING FILE AND ZERO OUT THE NETTED CATEGORIES
-    #
-    # GROSS PROFIT OR LOSS IS A FUTURES ONLY CATEGORY AND IS NETTED INTO BALANCE PREVIOUS YEAR
-    #   THIS CAN STAY UNTOUCHED
-    #
-    # OPEN TRADE EQUITY IS LISTED AS OPEN TRADE EQUITY FUTURES BUT STAYS THE SAME
-    #   THIS NEEDS TO BE RENAMED FOR VALUES TO COME THROUGH
+try:
+    from MonthEnd.Abn.Base import get_archive_date_path, set_globals
+    from MonthEnd.Abn.EoyExtractPageText import *
+    from MonthEnd.Abn import FileGrabber
+except ModuleNotFoundError:
+    from Base import get_archive_date_path, set_globals
+    from EoyExtractPageText import *
+    import FileGrabber
+
+
+def convert_to_eoy_cash(year: int) -> pd.DataFrame:
+    """Converts the last CSV Cash File of the year to a usable format for the
+    new year.
+
+    PROCESS NOTES
+    MARK TO MARKET OPTIONS LONG/SHORT ARE NETTED TO MARK TO MARKET OPTIONS
+
+    MARK TO MARKET UNSETTLED LONG/SHORT STOCK ARE NETTED TO MARK TO MARKET
+    UNSETTLED STOCK
+
+    MARK TO MARKET UNSETTLED LONG/SHORT PREFSTOCK ARE NETTED TO MARK TO
+    MARKET UNSETTLED PREFSTOCK
+      ADD PRIOR VALUES FOR THESE TO THE RESULTING FILE AND ZERO OUT THE
+      NETTED CATEGORIES
+
+    GROSS PROFIT OR LOSS IS A FUTURES ONLY CATEGORY AND IS NETTED INTO BALANCE PREVIOUS YEAR
+      THIS CAN STAY UNTOUCHED
+
+    OPEN TRADE EQUITY IS LISTED AS OPEN TRADE EQUITY FUTURES BUT STAYS THE
+    SAME
+      THIS NEEDS TO BE RENAMED FOR VALUES TO COME THROUGH
+    """
+
+    set_globals(year, 12, "C:/gdrive")
 
     dir = get_archive_date_path(day=31)
+    downloads = os.getenv("HOMEPATH") + "/Downloads"
 
-    f_pattern = r'[\w_\d]*DPR_SU_EOY.pdf'
+    f_pattern = r"[\w_\d]*DPR_SU_EOY.pdf"
 
-    proper_files = get_ABN_pdfs(year, 12, f_pattern)
+    proper_files = get_ABN_pdfs(f_pattern)
 
     os.chdir(dir)
 
-    dfs = []
+    master = pd.DataFrame()
     count = 0
 
     for f in proper_files:
@@ -38,73 +60,115 @@ def convert_to_eoy_cash(year: int, pm_cash: pd.DataFrame):
         for num in page_nums:
             page_obj = pages[num]
             df = get_data_table(f, page_obj)
-            dfs.append(df)
+            if count == 1:
+                master = df.copy(deep=True)
+            else:
+                master = pd.concat([master, df])
 
-    summary_df = pd.concat(dfs)
+    master.reset_index(drop=True, inplace=True)
 
     initial_line_drops = [
-        'CASH POSITON',
-        'NET LIQUIDATION',
-        'NET PROFIT & LOSS'
+        "CASH POSITION",
+        "NET LIQUIDATION",
+        "NET PROFIT & LOSS",
     ]
 
-    drop_index = summary_df.loc[summary_df['Cash Title'].isin(initial_line_drops)].index
-    summary_df = summary_df.drop(index=drop_index).reset_index(drop=True)
-    summary_df.loc[summary_df['Cash Title'].str.contains('MARGIN'), 'New'] = 0
-    total_sum = summary_df['New'].sum()
+    drop_index = master.loc[
+        master["Cash Title"].isin(initial_line_drops)
+    ].index
+    no_bad_lines = master.drop(index=drop_index).reset_index(drop=True)
+    no_bad_lines.loc[
+        no_bad_lines["Cash Title"].str.contains("MARGIN"), "New"
+    ] = 0
+    total_sum = no_bad_lines["New"].sum()
 
-    summary_df.to_csv('C:/Users/phugh/Downloads/summary_df.csv')
-
+    no_bad_lines.to_csv(downloads + "/summary_df.csv")
 
     ## + Formatting rules to make = to normal cash file ##
-    csv_cash = pm_cash
-    csv_cash.loc[csv_cash['Cash Title'].str.contains('MARGIN'), 'Opening Balance'] = 0
-    csv_cash_total = csv_cash['Opening Balance'].sum()
+    csv_cash = FileGrabber.get_cash_file(year, 12)
+    csv_cash.loc[
+        csv_cash["Cash Title"].str.contains("MARGIN"), "Opening Balance"
+    ] = 0
+    csv_cash_total = csv_cash["Opening Balance"].sum()
 
     ## Check 1 ##
     if round(total_sum, 0) != round(csv_cash_total, 0):
-        raise ValueError('Total Sum and CSVCASH Sum are not equal', f'Total Sum: {total_sum}', f'CSVCASH Sum: {csv_cash_total}')
+        raise ValueError(
+            "Total Sum and CSVCASH Sum are not equal",
+            f"Total Sum: {total_sum:,.2f}",
+            f"CSVCASH Sum: {csv_cash_total:,.2f}",
+        )
+    else:
+        print(
+            "Check 1 passed\n",
+            f"Total Sum: {total_sum:,.2f}",
+            f"CSVCASH Sum: {csv_cash_total:,.2f}",
+        )
 
     line_items_to_keep = [
-        'MARK TO MARKET OPTIONS LONG',
-        'MARK TO MARKET OPTIONS SHORT',
-        'MARK TO MARKET UNSETTLED LONG STOCK',
-        'MARK TO MARKET UNSETTLED SHORT STOCK',
-        'MARK TO MARKET UNSETTLED LONG PREFSTOCK',
-        'MARK TO MARKET UNSETTLED SHORT PREFSTOCK'
+        "MARK TO MARKET OPTIONS LONG",
+        "MARK TO MARKET OPTIONS SHORT",
+        "MARK TO MARKET UNSETTLED LONG STOCK",
+        "MARK TO MARKET UNSETTLED SHORT STOCK",
+        "MARK TO MARKET UNSETTLED LONG PREFSTOCK",
+        "MARK TO MARKET UNSETTLED SHORT PREFSTOCK",
     ]
 
-    mark_to_market = csv_cash.loc[csv_cash['Cash Title'].isin(line_items_to_keep)].copy()
-    mark_to_market = mark_to_market[['Account', 'Cash Title', 'Opening Balance']]
-    mark_to_market = mark_to_market.rename(columns={'Opening Balance', 'New'})
-    mark_to_market['Change'] = 0
-    mark_to_market['Old'] = mark_to_market['New']
-    mark_to_market['Month to Date'] = 0
-    mark_to_market['f_name'] = 'CSVCASH'
+    orig_mark_to_market = csv_cash.loc[
+        csv_cash["Cash Title"].isin(line_items_to_keep)
+    ].copy()
+    basic_mark_to_market = orig_mark_to_market[
+        ["Account", "Cash Title", "Opening Balance"]
+    ].copy(deep=True)
+    mark_to_market = basic_mark_to_market.rename(
+        columns={"Opening Balance": "New"}
+    ).copy(deep=True)
+    mark_to_market["Change"] = 0
+    mark_to_market["Old"] = mark_to_market["New"]
+    mark_to_market["Month to Date"] = 0
+    mark_to_market["f_name"] = "CSVCASH"
 
     line_items_to_drop = [
-        'MARK TO MARKET OPTIONS',
-        'MARK TO MARKET UNSETTLED STOCK',
-        'MARK TO MARKET UNSETTLED PREFSTOCK'
+        "MARK TO MARKET OPTIONS",
+        "MARK TO MARKET UNSETTLED STOCK",
+        "MARK TO MARKET UNSETTLED PREFSTOCK",
     ]
 
-    rows_to_drop = summary_df.loc[summary_df['Cash Title'].isin(line_items_to_drop)].index
-    summary_without_net_mtm = summary_df.drop(index=rows_to_drop).copy()
-    summary_with_split_mtm = pd.concat([summary_without_net_mtm, mark_to_market])
+    rows_to_drop = no_bad_lines.loc[
+        no_bad_lines["Cash Title"].isin(line_items_to_drop)
+    ].index
+    summary_without_net_mtm = no_bad_lines.drop(index=rows_to_drop).copy()
+    summary_with_split_mtm = pd.concat(
+        [summary_without_net_mtm, mark_to_market]
+    )
 
-    new_summary = summary_with_split_mtm.mask(summary_with_split_mtm['Cash Title'] == 'OPEN TRADE EQUITY FUTURES', 'OPEN TRADE EQUITY')
+    new_summary = summary_with_split_mtm.mask(
+        summary_with_split_mtm["Cash Title"] == "OPEN TRADE EQUITY FUTURES",
+        "OPEN TRADE EQUITY",
+    )
 
-    new_summary_final = new_summary.rename(columns={'New': 'Opening Balance'})
-    final_checksum = new_summary_final['Opening Balance'].sum()
+    new_summary_final = new_summary.rename(columns={"New": "Opening Balance"})
+    final_checksum = new_summary_final["Opening Balance"].sum()
+    new_summary_final.to_csv(downloads + "/final_df.csv")
 
     ## Check 2 ##
     if round(csv_cash_total, 0) != round(final_checksum, 0):
-        raise ValueError('Final Check Sum and CSVCASH Sum are not equal', f'Final Check Sum: {final_checksum}', f'CSVCASH Sum: {csv_cash_total}')
-    
+        raise ValueError(
+            "Final Check Sum and CSVCASH Sum are not equal",
+            f"Final Check Sum: {final_checksum:,.2f}",
+            f"CSVCASH Sum: {csv_cash_total:,.2f}",
+        )
+    else:
+        print(
+            "Check 2 passed\n",
+            f"Final Checksum: {final_checksum:,.2f}",
+            f"CSVCASH Sum: {csv_cash_total:,.2f}",
+        )
 
     return new_summary_final
 
-def get_ABN_pdfs( f_pattern):
+
+def get_ABN_pdfs(f_pattern):
     dir_path = get_archive_date_path()
 
     files = os.listdir(dir_path)
@@ -112,3 +176,11 @@ def get_ABN_pdfs( f_pattern):
     proper_files = list(filter(lambda x: re.search(f_pattern, x), files))
 
     return proper_files
+
+
+if __name__ == "__main__":
+    year = int(sys.argv[1])
+    eoy_cash = convert_to_eoy_cash(year)
+    eoy_cash.to_csv(
+        os.getenv("HOMEPATH") + "/Downloads/eoy_cash_final.csv", index=False
+    )
